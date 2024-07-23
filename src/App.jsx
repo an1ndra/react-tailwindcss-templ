82a80f49bbcc83fba0378a0d758747296773bf34
Anindra <anindrakarmakar+git@proton.me>
2024-07-23 20:15:38 +0530

    Fix spelling

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl text-center text-red-700">
-        Welcome to Vite with TailwindCSS and REact
+        Welcome to Vite with TailwindCSS and React
       </h1>
     </>
   );
