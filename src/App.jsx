d35bfc248ec99500cb38d5290dc54938edc94f19
Anindra <anindrakarmakar+git@proton.me>
2024-08-08 20:21:16 +0530

    pure vanilla

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-  return (
-    <>
-      <h1 className='text-3xl text-center text-cyan-600 font-sans'>
-        Welcome to Vite with{' '}
-        <span className='text-indigo-700'>TailwindCSS 🌀</span> and{' '}
-        <span className='text-indigo-700'> React ⚛</span>
-      </h1>
-    </>
-  )
+export default function App() {
+  return <div>App</div>
 }
-
-export default App
